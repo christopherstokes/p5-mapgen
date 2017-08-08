082eef2ea9842aa390d179dc6f146065a0c23c2c
xkfngs <christopherstokes@gmail.com>
2017-08-07 20:42:48 -0700

    add initial files

---
+++ mapgen.js
@@ -0,0 +1,15 @@
+function preload() {
+}
+
+function setup() {
+	createCanvas(600, 480);
+}
+
+function draw() {
+	background(0);
+}
+
+function mouseClicked() {
+	if (mouseX > 0 && mouseX < width && mouseY < height && mouseY > 0) { 
+	}
+}
