c552cce8d093b28d1387393739c5a3d7206d9ab7
xkfngs <christopherstokes@gmail.com>
2017-08-08 09:11:37 -0700

    create simple 1d noise gen

--- mapgen.js
+++ mapgen.js
@@ -1,15 +1,40 @@
+function drawGraph(arr) {
+	for (var x=0; x<width; x++) {
+		stroke(255, 0, 255);
+		line(x*4, height, x*4, (height/2 - ((arr[x] || 0) * 100))); 
+	}
+}
+
+function updateGraph(arr) {
+	arr.push(noise(arr[arr.length] || random()));
+	if (arr.length*4 > width) {arr.shift();}
+}
+
+var currentGraph = [];
+var isLooping = false;
+
 function preload() {
 }
 
 function setup() {
-	createCanvas(600, 480);
+	createCanvas(300,300);
+	noLoop();
 }
 
 function draw() {
-	background(0);
+	background(32);
+	updateGraph(currentGraph);
+	drawGraph(currentGraph);
 }
 
 function mouseClicked() {
 	if (mouseX > 0 && mouseX < width && mouseY < height && mouseY > 0) { 
+		if (!isLooping) {
+			isLooping = true;
+			loop();
+		} else {
+			isLooping = false;
+			noLoop();
+		}
 	}
 }
